e85ca84ccdc53b7acbc64b5e65736dbad1a006a0
amnakifayat13 <hayatullahamna@gmail.com>
2024-05-09 22:46:07 +0500

    Day 69 Challenge

---
+++ enum.ts
@@ -0,0 +1,18 @@
+enum Months {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+  }
+  
+  const month: Months = Months.May;
+  
+  console.log(`This is month of ${Months[month]}`);
